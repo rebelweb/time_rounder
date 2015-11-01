af8c5fcb3d03fd8904742177fbd2bd02514f3cc5
Ryan <rebelwebdevelopment@gmail.com>
2015-10-31 23:23:06 -0500

    added documentation to all current methods

--- lib/time_rounder.rb
+++ lib/time_rounder.rb
@@ -1,8 +1,16 @@
 require "time_rounder/version"
 require "time_rounder/rounded_time_from_seconds"
 
+##
+# TimeRounder is a library to round time.
+
 module TimeRounder
+
+  ##
+  # Takes number of seconds and returns hours and partial hours in decimal form.
+
   def self.seconds_to_hours(seconds)
     TimeRounder::RoundedTimeFromSeconds.new(seconds).rounded_time
   end
+  
 end
