974e4b15496328c66f0ed4314e045988e215c0b7
Ryan Condron <rebelwebdevelopment@gmail.com>
2016-02-11 15:28:28 -0600

    added documentation to load_schedule

--- lib/time_rounder/load_schedule.rb
+++ lib/time_rounder/load_schedule.rb
@@ -1,7 +1,12 @@
 require 'time_rounder/schedule/fifteen_minute'
 
 module TimeRounder
+  ##
+  # Handles loading of the proper schedule
   module LoadSchedule
+
+    ##
+    # loads the proper schedule based what minute is passed
     def get_schedule(minutes)
       case minutes
       when 15
